830ef1a49dbf0fe1aeb86d21a1ff5ae31157ffdf
Yascha Franz <yascha.franz@tu-dortmund.de>
2019-12-10 13:21:09 +0100

    plot.py in Default geupdated

--- Default/scripts/plot.py
+++ Default/scripts/plot.py
@@ -2,17 +2,21 @@ import matplotlib.pyplot as plt
 import numpy as np
 from scipy.optimize import curve_fit
 
-def plotfit(x,y,f,savepath):
-    plt.plot(x,y, 'k.', label ='Messwerte')
-    params, covariance_matrix = curve_fit(f, x, y, p0 = (1,1))
+def plotfit(x,y,f,savepath,slice_=slice(0,None),yerr=None, p0=None):
+    if yerr==None:
+        plt.plot(x,y, 'k.', label ='Messwerte')
+    else:
+        plt.errorbar(x,y,yerr=yerr, fmt='kx', label ='Messwerte')
+    params, covariance_matrix = curve_fit(f, x[slice_], y[slice_], p0=p0)
     errors = np.sqrt(np.diag(covariance_matrix))
-    x_plot = np.linspace(np.min(x), np.max(x), 1000)
+    x_plot = np.linspace(np.min(x[slice_]), np.max(x[slice_]), 1000)
     plt.plot(x_plot, f(x_plot, *params), 'b-', label=f.__name__)
     plt.legend(loc='best')
     plt.tight_layout(pad=0, h_pad=1.08, w_pad=1.08)
 
     plt.savefig(savepath)
     plt.clf()
+    return params, errors
 
 def polarplot(x,y,f,savepath):
     ax=plt.figure().gca(polar=True)
