ad057b4822168397f1d6633b7d679e84fbeccca0
Yascha Franz <yascha.franz@tu-dortmund.de>
2019-12-15 23:08:51 +0100

    Default plot.py verbessert

--- Default/scripts/plot.py
+++ Default/scripts/plot.py
@@ -2,20 +2,30 @@ import matplotlib.pyplot as plt
 import numpy as np
 from scipy.optimize import curve_fit
 
-def plotfit(x,y,f,savepath,slice_=slice(0,None),yerr=None, p0=None):
-    if yerr==None:
-        plt.plot(x,y, 'k.', label ='Messwerte')
+def plotfit(x,y,f,savepath,slice_=slice(0,None),yerr=None, p0=None, save=True, color='k'):
+    colors = ['k', 'b', 'g', 'r', 'y']
+    if (np.size(x[0])>1):
+        param, error = plotfit(x[0],y[0],f,savepath,slice_=slice_,yerr=yerr[0], p0=p0, save = False, color=colors[0])
+        params = [param]
+        errors = [error]
+        for i in range(1,np.shape(x)[0]):
+            param, error = plotfit(x[i],y[i],f,savepath,slice_=slice_,yerr=yerr[i], p0=p0, save = False, color=colors[i])
+            params = np.append(params, [param], axis = 0)
+            errors = np.append(errors, [error], axis = 0)
     else:
-        plt.errorbar(x,y,yerr=yerr, fmt='kx', label ='Messwerte')
-    params, covariance_matrix = curve_fit(f, x[slice_], y[slice_], p0=p0)
-    errors = np.sqrt(np.diag(covariance_matrix))
-    x_plot = np.linspace(np.min(x[slice_]), np.max(x[slice_]), 1000)
-    plt.plot(x_plot, f(x_plot, *params), 'b-', label=f.__name__)
-    plt.legend(loc='best')
-    plt.tight_layout(pad=0, h_pad=1.08, w_pad=1.08)
-
-    plt.savefig(savepath)
-    plt.clf()
+        if yerr is None:
+            plt.plot(x,y, color=color, linestyle='', marker='.', label ='Messwerte')
+        else:
+            plt.errorbar(x,y,yerr=yerr, color=color, linestyle='', marker='x', label ='Messwerte')
+        params, covariance_matrix = curve_fit(f, x[slice_], y[slice_],p0=p0)
+        errors = np.sqrt(np.diag(covariance_matrix))
+        x_plot = np.linspace(np.min(x[slice_]), np.max(x[slice_]), 1000)
+        plt.plot(x_plot, f(x_plot, *params), color=color, linestyle='-', label=f.__name__)
+        plt.legend(loc='best')
+        plt.tight_layout(pad=0, h_pad=1.08, w_pad=1.08)
+    if save:
+        plt.savefig(savepath)
+        plt.clf()
     return params, errors
 
 def polarplot(x,y,f,savepath):
